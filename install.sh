f94398c66d87cc9ef179f0419923235cdbe86914
alonehacker <aloneskiddie@gmail.com>
2021-12-22 16:22:03 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,33 @@
+
 #!/bin/bash
- sleep 2 && clear 
+ sleep 2 && clear
 echo   "                   1nstalling  basic packages      "
-echo   "-------------------------------------------------------------------"
-          apt-get update && apt-get upgrade && pkg update && pkg upgrade 
+echo   "----------------------------------------------------------------"
+          apt-get update && apt-get upgrade && pkg update && pkg upgrade
           clear
 echo   "                       1nstalling python      "
-echo   "-------------------------------------------------------------------"
-         apt-get install python python2 -y && sleep 2.5 && clear 
-echo   "                   1nstalling git wget curl                "
-echo   "-------------------------------------------------------------------"
-         apt-get install git wget curl -y && sleep 2.5 &&  clear
+echo   "-----------------------------------------------------------------"
+         apt-get install python python2 -y && sleep 1.5 && clear
+echo   "                   1nstalling git,wget,curl                "
+echo   "-----------------------------------------------------------------"
+         apt-get install git wget curl -y && sleep 1.5 &&  clear
 echo   "                        1nstalling pifiglet                "
-echo   "-------------------------------------------------------------------"
-         pip install pyfiglet 
-         sleep 2.5 
+echo   "-----------------------------------------------------------------"
+         pip install pyfiglet
+         sleep 1.5
          clear
-         sleep 3
+         sleep 1.5
 echo   "                                                             "
 echo   "                                                             "
-echo   " your basic packages for running tmsfi tool is installed " 
-         sleep 3 
+echo   " *your basic packages for running tmsfi tool is installed* "
+         sleep 2
 echo   "                                                                "
 clear
-echo   " downloading tmsfi file ...." 
+echo -e " *downloading tmsfi file ..."
 sleep 3
-cd ~ && wget https://raw.githubusercontent.com/alone-skiddie/tmsf/main/tmsfi.py
-python tmsfi.py
-
-
+echo "  --------------------------------------------------------------"
+cd ~ && wget https://raw.githubusercontent.com/alone-skiddie/tmsfi/main/tmsfi.py
+sleep 2 && clear
+echo " *sucessfully installed tmsfi file * "
+sleep 1.5
+python $HOME/tmsfi.py
